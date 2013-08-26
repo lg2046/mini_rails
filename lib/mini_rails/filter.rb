ef7e8fd477f0e3c20f17a2d6f0665e1b8d3824bd
lg2046 <lg2046@gmail.com>
2013-08-26 18:07:15 +0800

    增加直接使用activemodel 的callback的东西

--- lib/mini_rails/filter.rb
+++ lib/mini_rails/filter.rb
@@ -39,3 +39,41 @@ module MiniRails
     end
   end
 end
+
+# active model callback 版
+
+# module MiniRails
+#   module Filter
+#     extend ActiveSupport::Concern
+# 
+#     included do
+#       # class_attribute :before_filters, :after_filters, :around_filters
+#       # self.before_filters = []
+#       # self.after_filters = []
+#       # self.around_filters = []
+#       include ActiveSupport::Callbacks
+#        define_callbacks :filter
+#     end
+# 
+#     module ClassMethods
+#       def before_filter(filter)
+#         set_callback :filter, :before, filter
+#       end
+# 
+#       def after_filter(filter)
+#         set_callback :filter, :after, filter
+#       end
+# 
+#       def around_filter(filter)
+#         set_callback :filter, :around, filter
+#       end
+# 
+#     end
+# 
+#     def filter(&block)
+#       run_callbacks :filter do
+#         block.call
+#       end
+#     end
+#   end
+# end
